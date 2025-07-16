238fe99cedf965436f8172ded86ddc216c9da2e0
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-07-16 20:47:55 +0200

    modified: model

--- plugins/os-deepinspector/src/opnsense/mvc/app/controllers/OPNsense/DeepInspector/SettingsController.php
+++ plugins/os-deepinspector/src/opnsense/mvc/app/controllers/OPNsense/DeepInspector/SettingsController.php
@@ -1,19 +1,52 @@
 <?php
 
+/*
+ * Copyright (C) 2025 OPNsense Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 namespace OPNsense\DeepInspector;
 
+use OPNsense\Base\IndexController as BaseIndexController;
+
 /**
- * Settings Controller - DPI configuration interface
+ * Class SettingsController
+ * @package OPNsense\DeepInspector
  */
-class SettingsController extends \OPNsense\Base\IndexController
+class SettingsController extends BaseIndexController
 {
+    /**
+     * Deep Packet Inspector settings page
+     * @throws \Exception
+     */
     public function indexAction()
     {
         $this->view->title = gettext('Deep Packet Inspector - Settings');
-        $this->view->generalForm = $this->getForm('general');
-        $this->view->protocolsForm = $this->getForm('protocols');
-        $this->view->detectionForm = $this->getForm('detection');
-        $this->view->advancedForm = $this->getForm('advanced');
+        $this->view->generalForm = $this->getForm("general");
+        $this->view->protocolsForm = $this->getForm("protocols");
+        $this->view->detectionForm = $this->getForm("detection");
+        $this->view->advancedForm = $this->getForm("advanced");
         $this->view->pick('OPNsense/DeepInspector/settings');
     }
 }
