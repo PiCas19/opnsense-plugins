4906a7d26832ec471bb443b0ef2e2486403eb789
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-07-17 13:47:34 +0200

    modified: model

--- plugins/os-deepinspector/src/opnsense/mvc/app/controllers/OPNsense/DeepInspector/IndexController.php
+++ plugins/os-deepinspector/src/opnsense/mvc/app/controllers/OPNsense/DeepInspector/IndexController.php
@@ -14,12 +14,15 @@ namespace OPNsense\DeepInspector;
 class IndexController extends \OPNsense\Base\IndexController
 {
     /**
-     * deep inspector index page - COPIA ESATTA DI MONIT
+     * deep inspector index page con tutti i tabs
      * @throws \Exception
      */
     public function indexAction()
     {
         $this->view->formGeneralSettings = $this->getForm("general");
+        $this->view->formProtocolsSettings = $this->getForm("protocols");
+        $this->view->formDetectionSettings = $this->getForm("detection");
+        $this->view->formAdvancedSettings = $this->getForm("advanced");
         $this->view->pick('OPNsense/DeepInspector/index');
     }
 }
