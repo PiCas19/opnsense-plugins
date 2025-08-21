2316d8a5cbeccefa0cbf1b2cebac33cc86156270
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-08-21 17:33:38 +0200

    added

--- opnsense-management-api/wrapper/src/routes/rules.js
+++ opnsense-management-api/wrapper/src/routes/rules.js
@@ -237,7 +237,6 @@ router.get('/:id', asyncHandler(async (req, res) => {
     }
 
     const rule = await OpnsenseService.getRule(id);
-    console.log(rule);
     if (!rule) {
       return res.status(404).json({ success: false, message: 'Regola non trovata in OPNsense' });
     }
