908f3e0bcad83f5943a5d873a4742d90769adc96
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-09-24 08:20:26 +0200

    fix

--- plugins/install_signed_plugins.sh
+++ plugins/install_signed_plugins.sh
@@ -8,7 +8,7 @@
 
 set -e
 
-PLUGIN_DIRS="os-advinspector os-validation-core"
+PLUGIN_DIRS="os-advinspector"
 REPO_NAME="localrepo"
 REPO_BASE="/usr/local/localrepo"
 REPO_DIR="${REPO_BASE}/packages"
